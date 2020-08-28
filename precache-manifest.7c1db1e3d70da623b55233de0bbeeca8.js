43438da0a320344c507aafe508608d3d8ef9e60e
Vlad <vladhalchyshak@gmail.com>
2020-08-28 13:12:46 +0300

    Updates

---
+++ precache-manifest.7c1db1e3d70da623b55233de0bbeeca8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7856b7c52ee3606b60d31509e897095",
+    "url": "/Sushi_internetShop/index.html"
+  },
+  {
+    "revision": "096506a23a8b2f93f4d9",
+    "url": "/Sushi_internetShop/static/css/main.13acb749.chunk.css"
+  },
+  {
+    "revision": "bf65fc0032ede00ce1fe",
+    "url": "/Sushi_internetShop/static/js/2.b5b0b4e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Sushi_internetShop/static/js/2.b5b0b4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096506a23a8b2f93f4d9",
+    "url": "/Sushi_internetShop/static/js/main.951a917d.chunk.js"
+  },
+  {
+    "revision": "1e64da89ac02a08c6b74",
+    "url": "/Sushi_internetShop/static/js/runtime-main.7be134d8.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/Sushi_internetShop/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "efc14534e0eed34c13ff42e0500df974",
+    "url": "/Sushi_internetShop/static/media/california_crab.efc14534.jpg"
+  },
+  {
+    "revision": "a219d84c99fd2a24dd6b855fe3469a5e",
+    "url": "/Sushi_internetShop/static/media/empty.a219d84c.png"
+  }
+]);
